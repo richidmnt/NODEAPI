70204c4a5d9809cd5944eeaa070c4f489dce2195
richidmnt <137005300+richidmnt@users.noreply.github.com>
2024-01-07 20:13:32 -0500

    host

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import {createPool} from 'mysql2/promise'
 import {DB_USER,DB_PORT,DB_HOST,DB_DATABASE,DB_PASSWORD} from './config.js'
 export const pool = createPool({
-    host:"railway",
+    host:"monorail.proxy.rlwy.net",
     user:"root",
     password:"d4h2ABgG-6bFbb5hB2aA-4hcH-FD5b-F",
     port:49639,
