2962af31702dfcc51bb1ca1223a1d29839243ecb
richidmnt <137005300+richidmnt@users.noreply.github.com>
2024-01-07 20:38:48 -0500

    cambio de host

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import {DB_USER,DB_PORT,DB_HOST,DB_DATABASE,DB_PASSWORD} from './config.js'
 export const pool = createPool({
     host:"monorail.proxy.rlwy.net",
     user:"root",
-    password:"d4h2ABgG-6bFbb5hB2aA-4hcH-FD5b-F",
-    port:49639,
+    password:"ef5fDaHfgDEgebh3EFgeEDbDaea2-c2g",
+    port:44517,
     database:"railway"
 })
